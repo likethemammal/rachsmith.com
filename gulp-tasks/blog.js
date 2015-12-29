e685627aa5c072d125191e676948534a27d73678
Rachel Smith <contact@rachsmith.com>
2015-12-29 14:33:09 -0800

    add prev and next links to post

--- gulp-tasks/blog.js
+++ gulp-tasks/blog.js
@@ -9,21 +9,33 @@ module.exports = function (gulp, rss, md) {
             fs.readdir('site/content/posts', function (err, files) {
                 var scripts = fs.readFileSync('site/content/templates/shared/scripts.html', 'utf8');
                 var footer = fs.readFileSync('site/content/templates/shared/footer.html', 'utf8');
-                var allPosts = [];
+                var allPosts = [], writtenPosts = [];
                 for (var i = 0, l = files.length; i < l; i++) {
                     md.process(files[i], 'posts', function (post) {
-                        if (post.settings.published == 'true') posts.push(post);
-                        if (post.settings.type != 'link' && post.settings.type != 'codepen') generateHTML(post, 'posts');
                         post.settings.scripts = scripts;
                         post.settings.footer = footer;
-                        console.log(post.settings);
+                        if (post.settings.published == 'true') posts.push(post);
+                        if (post.settings.type != 'link' && post.settings.type != 'codepen') {
+                            writtenPosts.push(post);
+                        }
                         allPosts.push(post);
                         processed++;
                         if (processed == files.length) {
+                            createHTML();
                             rss.create(allPosts);
                             createBlog(posts);
                         }
                     });
+                }
+
+                function createHTML() {
+                    for (var i = 0; i < writtenPosts.length; i++) {
+                        if (writtenPosts[i-1] && writtenPosts[i-1].settings.url) writtenPosts[i].settings['prev-url'] = writtenPosts[i-1].settings.url;
+                        if (writtenPosts[i-1] && writtenPosts[i-1].settings.title) writtenPosts[i].settings['prev-title'] = writtenPosts[i-1].settings.title;
+                        if (writtenPosts[i+1] && writtenPosts[i+1].settings.url) writtenPosts[i].settings['next-url'] = writtenPosts[i+1].settings.url;
+                        if (writtenPosts[i+1] && writtenPosts[i+1].settings.title) writtenPosts[i].settings['next-title'] = writtenPosts[i+1].settings.title;
+                        generateHTML(writtenPosts[i], 'posts');
+                    }
                 }
             });
         },
@@ -42,6 +54,7 @@ module.exports = function (gulp, rss, md) {
 function generateHTML(post, contentType) {
     fs.readFile('site/content/templates/'+contentType+'/'+post.settings.type+'.html', 'utf8', function(err, template) {
         var toReplace = template.match(new RegExp('\{{(.*?)\}}', 'g'));
+        console.log(post.settings);
         for(var i = 0, l = toReplace.length; i < l; i++) {
             template = template.replace(toReplace[i], post.settings[toReplace[i]
                     .replace(new RegExp('\{{|\}}', 'g'), '')] || '');
